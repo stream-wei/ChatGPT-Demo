4b9502fb45cf297c4e3d0b770690d7820d598dd5
xi.wei <xi.wei@huifu.com>
2023-06-02 19:05:07 +0800

    flows review

--- src/main/java/com/stream/PrimeTest.java
+++ src/main/java/com/stream/PrimeTest.java
@@ -2,8 +2,10 @@ package com.stream;
 
 public class PrimeTest {
 
+    public static void main(String[] args) {
+    }
 
-    public boolean isPrime1(int n){
+    public static boolean isPrime1(int n){
         for (int i = 2; i < n; i++) {
             if (n % i == 0){
                 return true;
